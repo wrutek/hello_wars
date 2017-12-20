afa7c4a8aafd77b502ff5cb872ff57a132ace7d7
Wiktor Rutka <wiktor.rutka@intive.com>
2017-12-20 14:47:29 +0100

    [#1] add db and migrations
     - update gitignore
     - remove cached pyc file
     - add map model migration

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Map(models.Model):
+    """ Map
+    Map model is handling information about single defined map
+    """
+    rows = models.IntegerField()
+    cols = models.IntegerField()
+    text_definition = models.TextField()
